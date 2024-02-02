97bb33f9fae0e5f5b07df559bda7be18fce3bfc9
HISOKA_AN <qfengqin@gmail.com>
2024-02-03 02:01:35 +0800

    helpcenter

---
+++ app/src/main/java/com/example/travel_itinerary_planner/chat_email/EmailSupportActivity.kt
@@ -0,0 +1,13 @@
+package com.example.travel_itinerary_planner.chat_email
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.travel_itinerary_planner.R
+
+class EmailSupportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_support)
+
+    }
+}
